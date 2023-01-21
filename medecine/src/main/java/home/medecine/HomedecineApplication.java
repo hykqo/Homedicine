f1ebb4a0d9f493eed1438a7232269a00ffc624f3
ENOCH YEOM <hykqo@naver.com>
2023-01-21 20:22:09 +0900

    초기 gradle 설정

---
+++ medecine/src/main/java/home/medecine/HomedecineApplication.java
@@ -0,0 +1,12 @@
+package home.medecine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedecineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomedecineApplication.class, args);
+	}
+}
---
+++ medecine/src/test/java/home/medecine/HomedecineApplicationTests.java
@@ -0,0 +1,13 @@
+package home.medecine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomedecineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
