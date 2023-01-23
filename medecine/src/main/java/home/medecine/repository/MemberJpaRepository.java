bcd0d02c3483ab85014ed963a5c49e2f18952f4c
ENOCH YEOM <hykqo@naver.com>
2023-01-23 14:06:08 +0900

    spring security - 셋팅 (#1)

---
+++ medecine/src/main/java/home/medecine/repository/MemberJpaRepository.java
@@ -0,0 +1,9 @@
+package home.medecine.repository;
+
+import home.medecine.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberJpaRepository extends JpaRepository<Member, Long> {
+}
